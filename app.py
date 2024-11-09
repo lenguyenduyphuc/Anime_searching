721cfebc3259d77990b2b73e00e065450b169927
Phuc <duyphuclenguyen@gmail.com>
2024-11-09 15:22:23 -0500

    Make a database

--- app.py
+++ app.py
@@ -1,36 +1,39 @@
 from flask import Flask, jsonify, render_template, request
 import requests
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///anime.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Anime(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    synopsis = db.Column(db.Text, nullable=True)
+    score = db.Column(db.Float, nullable=True)
+
+def insert_data(data):
+    for item in data['data']:
+        anime = Anime.query.get(item['mal_id'])
+        if not anime:  # Avoid duplicates
+            anime = Anime(
+                id=item['mal_id'],
+                title=item['title'],
+                synopsis=item.get('synopsis'),
+                score=item.get('score')
+            )
+            db.session.add(anime)
+    db.session.commit()
 
 @app.route('/')
-def home():
-    return "Welcome to my anime searching"
+def index():
+    response = requests.get('https://api.jikan.moe/v4/anime')
+    data = response.json()
+    insert_data(data)
+    return "Data inserted into the database!"
 
-@app.route('/api/data', methods=["GET"])
-def get_data():
-    try: 
-        response = requests.get('https://api.jikan.moe/v4/anime')
-
-        response.raise_for_status()
-        data = response.json()
-
-    except requests.exceptions.HTTPError as http_err:
-        return jsonify({'error': f'HTTP error occurred: {http_err}'}), 500
-    except Exception as err:
-        return jsonify({'error': f'Other error occurred: {err}'}), 500
-    print(response)
-    return render_template('data.html', data=jsonify(data))
-
-@app.route('/returnjson', methods = ['GET']) 
-def ReturnJSON(): 
-    if (request.method == 'GET'): 
-        response = requests.get('https://api.jikan.moe/v4/anime')
-
-        response.raise_for_status()
-        data = response.json()
-
-        return jsonify(data) 
-  
 if __name__ == '__main__':
-    app.run(debug = True)
+    with app.app_context():  # Ensures db.create_all() works within an app context
+        db.create_all()
+    app.run(debug=True)
